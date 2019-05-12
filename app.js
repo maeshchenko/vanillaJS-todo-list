5733c994f67438c1b603d59e354f348e96ce40ae
mikhail.eshchenko <mikhail.eshchenko@gmail.com>
2019-05-12 20:40:23 +0300

    filter tasks

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ function loadEventListeners(){
   //remove task event
   taskList.addEventListener('click',removeTask);
   //clear tasks event
-  clearBtn.addEventListener('click',clearTasks)
+  clearBtn.addEventListener('click',clearTasks);
+  //filter tasks event
+  filter.addEventListener('keyup',filterTasks);
 }
 
 //add task
@@ -62,4 +64,21 @@ function clearTasks(e){
   while(taskList.firstChild){
     taskList.removeChild(taskList.firstChild);
   }
+}
+
+//filter tasks
+function filterTasks(e){
+  const text = e.target.value.toLowerCase();
+  document.querySelectorAll('.collection-item').forEach(function(task){
+    const item = task.firstChild.textContent;
+    
+    if(item.toLowerCase().indexOf(text) != -1){
+      task.style.display = 'block'
+    }else{
+      task.style.display = 'none'
+    }
+  })
+
+  console.log(text);
+  
 }
