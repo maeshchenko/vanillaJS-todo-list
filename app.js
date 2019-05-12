dcd2599c4a7c1ec29006a4ae0af1be78ae2f7b5f
mikhail.eshchenko <mikhail.eshchenko@gmail.com>
2019-05-12 17:59:20 +0300

    add tasks

--- app.js
+++ app.js
@@ -1,6 +1,47 @@
-//Define UI Vars
+//define UI vars
 const form = document.querySelector('form');
 const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
+
+//load all event listeners
+loadEventListeners();
+
+//load all event listeners
+function loadEventListeners(){
+  //add task event
+  form.addEventListener('submit', addTask);
+}
+
+//add task
+function addTask(e){
+  if(taskInput.value === ''){
+    alert('Add a task!');
+  }
+
+  // create li element
+  const li = document.createElement('li');
+  //add class
+  li.className = 'collection-item';
+  //create textNode and append to li
+  li.appendChild(document.createTextNode(taskInput.value));
+  //create new link element
+  const link = document.createElement('a');
+  //add class
+  link.className = 'delete-item secondary-content';
+  //add icon html
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  //append link to li
+  li.appendChild(link);
+
+  //append li to ul
+  taskList.appendChild(li);
+
+  //clear input
+  taskInput.value = '';
+
+
+
+  e.preventDefault();
+}
