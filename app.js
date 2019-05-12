f038dbf18c1c5044f15fd0837c4c0490792bf381
mikhail.eshchenko <mikhail.eshchenko@gmail.com>
2019-05-12 18:29:50 +0300

    remove rasks

--- app.js
+++ app.js
@@ -12,36 +12,45 @@ loadEventListeners();
 function loadEventListeners(){
   //add task event
   form.addEventListener('submit', addTask);
+  //remove task event
+  taskList.addEventListener('click',removeTask);
 }
 
 //add task
 function addTask(e){
   if(taskInput.value === ''){
     alert('Add a task!');
+  }else{
+    // create li element
+    const li = document.createElement('li');
+    //add class
+    li.className = 'collection-item';
+    //create textNode and append to li
+    li.appendChild(document.createTextNode(taskInput.value));
+    //create new link element
+    const link = document.createElement('a');
+    //add class
+    link.className = 'delete-item secondary-content';
+    //add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    //append link to li
+    li.appendChild(link);
+
+    //append li to ul
+    taskList.appendChild(li);
+
+    //clear input
+    taskInput.value = '';
   }
 
-  // create li element
-  const li = document.createElement('li');
-  //add class
-  li.className = 'collection-item';
-  //create textNode and append to li
-  li.appendChild(document.createTextNode(taskInput.value));
-  //create new link element
-  const link = document.createElement('a');
-  //add class
-  link.className = 'delete-item secondary-content';
-  //add icon html
-  link.innerHTML = '<i class="fa fa-remove"></i>';
-  //append link to li
-  li.appendChild(link);
-
-  //append li to ul
-  taskList.appendChild(li);
-
-  //clear input
-  taskInput.value = '';
-
-
-
   e.preventDefault();
+}
+
+//remove task
+function removeTask(e){
+  if(e.target.parentElement.classList.contains('delete-item')){
+    if(confirm('Are you sure?')){
+      e.target.parentElement.parentElement.remove();
+    }
+  }
 }
